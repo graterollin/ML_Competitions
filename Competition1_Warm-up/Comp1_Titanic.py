0410c2f720e380a99ce16c8f2dac4a9d1b360645
Andres Graterol <graterol.andres0@knights.ucf.edu>
2022-09-18 14:31:26 -0400

    New high score on Kaggle: 0.77

--- Competition1_Warm-up/Comp1_Titanic.py
+++ Competition1_Warm-up/Comp1_Titanic.py
@@ -11,10 +11,15 @@
 # data analysis and wrangling
 import pandas as pd
 
+# preprocessing
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+
 # machine learning
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import Perceptron
@@ -60,9 +65,19 @@ training_set = training_set.drop(['Name', 'PassengerId'], axis=1)
 testing_set = testing_set.drop(['Name'], axis=1)
 combine = [training_set, testing_set]
 
-# Binarization of sex
+# CONVERTING NON-NUMERIC DATA INTO NUMERIC DATA
+#Binarization of sex
 for dataset in combine:
     dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+# Completing Embarked and turning it to numeric
+freq_port = training_set.Embarked.dropna().mode()[0]
+
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+    
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
 
 # Filling gaps in Age
 #print("\nBefore inserting age")
@@ -71,9 +86,10 @@ for dataset in combine:
 # Interpolating missing age values separately 
 training_set['Age'] = training_set['Age'].interpolate()
 testing_set['Age'] = training_set['Age'].interpolate()
+combine = [training_set, testing_set]
 
-#print("After inserting age")
-#print(training_set.head(10))
+print("After inserting age")
+print(training_set.head(10))
 
 # Combining existing features parch and sibsp
 for dataset in combine:
@@ -87,15 +103,6 @@ training_set = training_set.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
 testing_set = testing_set.drop(['Parch', 'SibSp', 'FamilySize'], axis=1)
 combine = [training_set, testing_set]
     
-# Completing Embarked and turning it to numeric
-freq_port = training_set.Embarked.dropna().mode()[0]
-
-for dataset in combine:
-    dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
-    
-for dataset in combine:
-    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
-    
 # Completing fare in testing set then making it ordinal
 testing_set['Fare'].fillna(testing_set['Fare'].dropna().median(), inplace=True)  
 
@@ -108,9 +115,9 @@ for dataset in combine:
 
 combine = [training_set, testing_set]
 
-#print("\nFinal Working test and training sets:")
-#print('\n', training_set.head(10))
-#print('\n', testing_set.head(10))
+print("\nFinal Working test and training sets:")
+print('\n', training_set.head(10))
+print('\n', testing_set.head(10))
 
 # # ------ Model, predict and solve ------
 X_train = training_set.drop("Survived", axis=1)
@@ -118,7 +125,7 @@ X_train = training_set.drop("Survived", axis=1)
 Y_train = training_set["Survived"]
 #print("Shape of Y_train: ", Y_train.shape)
 X_test  = testing_set.drop("PassengerId", axis=1).copy()
-#print("Shape of X_test: ", X_test.shape)
+print("Shape of X_test: ", X_test.shape)
 
 # Logistic Regression 
 logreg = LogisticRegression()
@@ -137,54 +144,57 @@ knn = KNeighborsClassifier(n_neighbors = 3)
 knn.fit(X_train, Y_train)
 Y_pred = knn.predict(X_test)
 acc_knn = round(knn.score(X_train, Y_train) * 100, 2)
-print(acc_knn)
 
 # Gaussian Naive Bayes
 gaussian = GaussianNB()
 gaussian.fit(X_train, Y_train)
-Y_pred = gaussian.predict(X_test)
+#Y_pred = gaussian.predict(X_test)
 acc_gaussian = round(gaussian.score(X_train, Y_train) * 100, 2)
 
 # Perceptron
 perceptron = Perceptron()
 perceptron.fit(X_train, Y_train)
-Y_pred = perceptron.predict(X_test)
+#Y_pred = perceptron.predict(X_test)
 acc_perceptron = round(perceptron.score(X_train, Y_train) * 100, 2)
 
 # Linear SVC
 linear_svc = LinearSVC()
 linear_svc.fit(X_train, Y_train)
-Y_pred = linear_svc.predict(X_test)
+#Y_pred = linear_svc.predict(X_test)
 acc_linear_svc = round(linear_svc.score(X_train, Y_train) * 100, 2)
 
 # Stochastic Gradient Descent
 sgd = SGDClassifier()
 sgd.fit(X_train, Y_train)
-Y_pred = sgd.predict(X_test)
+#Y_pred = sgd.predict(X_test)
 acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
 
 # Decision Tree
 decision_tree = DecisionTreeClassifier()
 decision_tree.fit(X_train, Y_train)
-Y_pred = decision_tree.predict(X_test)
+#Y_pred = decision_tree.predict(X_test)
 acc_decision_tree = round(decision_tree.score(X_train, Y_train) * 100, 2)
 
 # Random Forest
-random_forest = RandomForestClassifier(n_estimators=100)
+random_forest = RandomForestClassifier(min_samples_leaf=52)
 random_forest.fit(X_train, Y_train)
 Y_pred = random_forest.predict(X_test)
 acc_random_forest = round(random_forest.score(X_train, Y_train) * 100, 2)
-print(acc_random_forest)
+
+# XGB
+gradient_boost = GradientBoostingClassifier()
+gradient_boost.fit(X_train, Y_train)
+acc_gradient_boost = round(gradient_boost.score(X_train, Y_train) * 100, 2)
 
 print('\n')
 models = pd.DataFrame({
     'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression', 
               'Random Forest', 'Naive Bayes', 'Perceptron', 
               'Stochastic Gradient Decent', 'Linear SVC', 
-              'Decision Tree'],
+              'Decision Tree', 'Gradient Boost'],
     'Score': [acc_svc, acc_knn, acc_log, 
               acc_random_forest, acc_gaussian, acc_perceptron, 
-              acc_sgd, acc_linear_svc, acc_decision_tree]})
+              acc_sgd, acc_linear_svc, acc_decision_tree, acc_gradient_boost]})
 print(models.sort_values(by='Score', ascending=False))
 
 submission = pd.DataFrame({
