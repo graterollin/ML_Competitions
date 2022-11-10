7a93607df25b89a754ed527a9ad013961e60a00b
graterollin <angels.andres0@gmail.com>
2022-11-10 00:20:12 -0500

    Handled missing data in the features

--- Competition2/Comp2_SpaceshipTitanic.py
+++ Competition2/Comp2_SpaceshipTitanic.py
@@ -25,5 +25,58 @@ combine = [training_set, testing_set]
 #print('_'*40)
 #testing_set.info()
 
+print(training_set.describe())
+print('\n')
 print(training_set.describe(include=['O']))
 
+# Interpolate missing numerical values 
+# Age
+training_set['Age'] = training_set['Age'].interpolate()
+testing_set['Age'] = testing_set['Age'].interpolate()
+# RoomService 
+training_set['RoomService'] = training_set['RoomService'].interpolate()
+testing_set['RoomService'] = testing_set['RoomService'].interpolate()
+# FoodCourt 
+training_set['FoodCourt'] = training_set['FoodCourt'].interpolate()
+testing_set['FoodCourt'] = testing_set['FoodCourt'].interpolate()
+# ShoppingMall
+training_set['ShoppingMall'] = training_set['ShoppingMall'].interpolate()
+testing_set['ShoppingMall'] = testing_set['ShoppingMall'].interpolate()
+# Spa
+training_set['Spa'] = training_set['Spa'].interpolate()
+testing_set['Spa'] = testing_set['Spa'].interpolate()
+# VRDeck
+training_set['VRDeck'] = training_set['VRDeck'].interpolate()
+testing_set['VRDeck'] = testing_set['VRDeck'].interpolate()
+
+combine = [training_set, testing_set]
+
+# Fill missing categorical attributes with the mode of each feature
+# CryoSleep
+freq_CryoSleep = training_set.CryoSleep.dropna().mode()[0]
+
+for dataset in combine:
+    dataset['CryoSleep'] = dataset['CryoSleep'].fillna(freq_CryoSleep)
+
+# VIP 
+freq_VIP = training_set.VIP.dropna().mode()[0]
+
+for dataset in combine:
+    dataset['VIP'] = dataset['VIP'].fillna(freq_VIP)
+
+# HomePlanet 
+freq_HomePlanet = training_set.HomePlanet.dropna().mode()[0]
+
+for dataset in combine:
+    dataset['HomePlanet'] = dataset['HomePlanet'].fillna(freq_HomePlanet)
+
+# Destination
+freq_Destination = training_set.Destination.dropna().mode()[0]
+
+for dataset in combine:
+    dataset['Destination'] = dataset['Destination'].fillna(freq_Destination)
+
+# Drop rows that we cannot fill in through statistical methods
+for dataset in combine:
+    dataset['Cabin'] = dataset['Cabin'].dropna()
+    dataset['Name'] = dataset['Name'].dropna()
